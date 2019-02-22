28c0316efbb30bc5085e08bd9acfc572af6828dd
André Hoendgen <hello@andre-hoendgen.de>
2019-02-22 10:33:28 +0100

    remove inital development artifacts and switch over to gitflow for a more structred approach

--- src/transform.ts
+++ src/transform.ts
@@ -1,13 +1,6 @@
 import preevaluationTransformer from './preevaluation';
 import prevalOptions from './prevalOptions';
-//import developmentTransformer from "./development";
-
-// const mode: string = process.env.NODE_ENV || "production";
 
 export default (options?: prevalOptions) => {
-  // if (mode === "production") {
   return preevaluationTransformer(options || {});
-  // } else {
-  // return  createTransformer = developmentTransformer(caching);
-  // }
 };
